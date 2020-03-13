4eed21eed3a67124949edbe973e741ef8ab928b0
shotwn <anilhaksever@gmail.com>
2020-03-14 00:44:08 +0300

    flake8 whitespace cleanup

--- thatradiothing/__init__.py
+++ thatradiothing/__init__.py
@@ -3,6 +3,7 @@ import thatradiothing.server
 import thatradiothing.master
 from thatradiothing.config import CONFIG
 
+
 class ThatRadioThing:
     def __init__(self):
         self.url = CONFIG['url']
@@ -19,7 +20,7 @@ class ThatRadioThing:
         self.web_server_task = None
         self.master = thatradiothing.master.Master(self)
         self.master_task = None
-    
+
     def run(self):
         loop = asyncio.get_event_loop()
         self.web_server_task = loop.create_task(self.web_server.run())
@@ -27,11 +28,11 @@ class ThatRadioThing:
         self.master_task = loop.create_task(self.master.beat())
         self.master_task.add_done_callback(self.aio_exception_handler)
         loop.run_forever()
-    
+
     def aio_exception_handler(self, future):
         if future.exception():
             future.result()
-    
+
     find_user_allowed_keys = [
         'session_id',
     ]
@@ -39,7 +40,7 @@ class ThatRadioThing:
     async def find_users(self, **kwargs):
         if not kwargs:
             raise ValueError()
-        
+
         for kw in kwargs:
             if kw not in self.find_user_allowed_keys:
                 raise KeyError()
@@ -52,12 +53,13 @@ class ThatRadioThing:
                     continue
                 break
             else:
-                match = True     
+                match = True
+
             if match:
                 found_users.append(user)
 
         return found_users
-    
+
     async def find_user(self, **kwargs):
         users = await self.find_users(**kwargs)
         if not users:
@@ -65,5 +67,3 @@ class ThatRadioThing:
 
         if len(users) == 1:
             return users[0]
-        
-        
